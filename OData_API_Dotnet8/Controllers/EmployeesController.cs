fe4b67376557b821bb5fb1675c497fba2d981b77
Jay Krishna <jay.krishna@consultant.volvo.com>
2023-12-02 10:10:33 +0100

    Final Changes

---
+++ OData_API_Dotnet8/Controllers/EmployeesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using OData_API_Dotnet8.Services;
+
+namespace OData_API_Dotnet8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+       private EmployeeService _employeeService;
+        public EmployeesController(EmployeeService employeeService)
+        {
+                _employeeService = employeeService;
+        }
+        [EnableQuery]
+        public IActionResult Get()
+        {
+            return Ok(_employeeService.GetEmployees());
+        }
+    }
+}
--- OData_API_Dotnet8/Program.cs
+++ OData_API_Dotnet8/Program.cs
@@ -1,14 +1,28 @@
+using Microsoft.AspNetCore.OData;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+using OData_API_Dotnet8.Models;
+using OData_API_Dotnet8.Services;
+
 namespace OData_API_Dotnet8
 {
     public class Program
     {
+        static IEdmModel GetEdmModel()
+        {
+            ODataConventionModelBuilder builder = new();
+            builder.EntitySet<Employee>("Employees");
+            return builder.GetEdmModel();
+        }
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers().AddOData(opt => opt.AddRouteComponents("v1", GetEdmModel()).Filter().Select().Expand().Count().OrderBy());
+
+            builder.Services.AddScoped<EmployeeService>();
 
             var app = builder.Build();
 
@@ -17,7 +31,6 @@ namespace OData_API_Dotnet8
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
